db53f9c8f76924c93d52084185e63dcac326f892
leek94 <khwan929@naver.com>
2026-03-25 22:03:32 +0900

    260325 알고리즘 문제 저장 (수정)

--- src/main/java/org/example/algorithm/inflearn/string/$6중복문자제거.java
+++ src/main/java/org/example/algorithm/inflearn/string/$6중복문자제거.java
@@ -9,6 +9,12 @@ public class $6중복문자제거 {
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
 
+//        indexOf를 사용해서 처음 나온 char만 저장해서 푸는 방식도있음, 2중 for문도 있음
+//        for( int i =0; i < str.length(); i++){
+//            System.out.println(str.charAt(i) +" "+ i +" "+str.indexOf(str.charAt(i)));
+//        }
+
+
         char[] arr = str.toCharArray();
         Set<Character> charSet = new LinkedHashSet<>();
 
@@ -23,7 +29,5 @@ public class $6중복문자제거 {
         }
 
         System.out.println(sb.toString());
-
-
     }
 }
